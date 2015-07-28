90afea5c7462a27f4dc1e932ed98bb1ef9c1f0f2
komastar <komastar.dev@gmail.com>
2015-07-28 22:04:43 +0900

    init project

---
+++ DaystoWebService/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DaystoWebService.Startup))]
+namespace DaystoWebService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
